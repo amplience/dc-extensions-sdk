dab19891d1ae0289c286757067d8a60288117eef
James Warner <jwarner@amplience.com>
2019-09-26 14:02:53 +0100

    fix: expose ids on schema interface

--- src/lib/models/ContentType.ts
+++ src/lib/models/ContentType.ts
@@ -1,32 +1,28 @@
 /**
  * This JSON Schema defines the data structure of your Content Type
  */
+
 export interface ContentType {
   id: string;
   contentTypeUri: string;
   settings: ContentTypeSettings;
-  schema: ContentTypeV4 | ContentTypeV7;
+  schema: DefaultSchemaProperties;
 }
-
 interface ContentTypeSettings {
   label: string;
   icons: Array<string>;
   cards: Array<string>;
   visualisation: Array<string>;
 }
-interface ContentTypeV4 extends DefaultSchemaProperties {
-  id: string;
-  schema: string;
-}
-
-interface ContentTypeV7 extends DefaultSchemaProperties {
-  $id: string;
-  $schema: string;
-}
 
 interface DefaultSchemaProperties {
+  /***  Version 7 json schema id */
+  $id?: string;
+  /***  Version 4 json schema id */
+  id?: string;
+  $schema: string;
   title: string;
-  description: string;
+  description?: string;
   type?: Types;
   allOf: Array<Object>;
   properties: ObjectMap<Object, ObjectMap<PropertyBody>>;
