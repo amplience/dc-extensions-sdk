986bbdfd3c4b83408bf230169cc08d8feef07d65
ccorbett <ccorbett@amplience.com>
2019-11-05 14:39:53 +0000

    refactor: renaming generic, updating typedoc

--- src/lib/ContentItem.ts
+++ src/lib/ContentItem.ts
@@ -4,14 +4,14 @@ import { ContentItemModel } from './models/ContentItemModel';
 
 export class ContentItem {
   /**
-   * Content Item - Used for fetching the model of the Content Item that is being edited.
+   * Content Item - Used for retrieving Content Items.
    * @param connection message.io connection
    */
   constructor(private connection: ClientConnection) {}
   /**
    * Use to fetch the Content Item that is currently being edited. Returns a promise which will resolve to a [[ContentItemModel]]
    */
-  async getCurrent<FormModel = {}>(): Promise<ContentItemModel<FormModel>> {
+  async getCurrent<ContentItemBody = {}>(): Promise<ContentItemModel<ContentItemBody>> {
     const contentItem = await this.connection.request(CONTENT_ITEM.GET);
     return contentItem;
   }
