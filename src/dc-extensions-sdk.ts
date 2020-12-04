c75c282804486bcdcf8b2314cff1690a1a98c871
Marc Easen <measen@amplience.com>
2020-12-04 16:11:27 +0000

    feat: exposed params type

--- src/dc-extensions-sdk.ts
+++ src/dc-extensions-sdk.ts
@@ -1,5 +1,6 @@
 export * from './lib/models/ContentItemModel';
 export * from './lib/models/ErrorReport';
+export * from './lib/models/Params';
 export * from './lib/models/ValidationError';
 export * from './lib/models/Locales';
 export * from './lib/MediaLink';
