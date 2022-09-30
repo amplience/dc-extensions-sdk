137127b7bc32344a9efc67d0e0918d735171127a
Jonny Cheetham <jcheetham@amplience.com>
2022-09-30 11:36:36 +0100

    bug: CMP-9538 Fixed build - Fixed TS errors and failing tests - Apparently 'hub' does not exist on ContextObject, even though it has a hub property so had to do a horrible type conversion to satisfy Typescript

--- src/lib/extensions/content-editor/ContentEditorExtension.spec.ts
+++ src/lib/extensions/content-editor/ContentEditorExtension.spec.ts
@@ -9,72 +9,64 @@ import { ContentLink } from '../../components/ContentLink';
 import { MediaLink } from '../../components/MediaLink';
 
 describe('ContentEditorExtension', () => {
+  const context = {
+    category: 'CONTENT_EDITOR',
+    contentItemId: '12345678-abcd-1234-1234-abcdef123456',
+    schema: {
+      id: 'http://simple.com',
+      $schema: 'http://json-schema.org/draft-07/schema#',
+      allOf: [
+        {
+          $ref: 'http://bigcontent.io/cms/schema/v1/core#/definitions/content',
+        },
+      ],
+      title: 'test-field-schema-title',
+      type: 'test-field-schema-type',
+      description: 'test-field-schema-desc',
+      properties: {
+        title: {
+          type: 'string',
+        },
+      },
+    },
+    params: {
+      instance: {},
+      installation: {},
+    },
+    locales: {
+      default: ['en'],
+      available: [{ locale: 'en-gb', language: 'en', country: 'gb', index: 1, selected: true }],
+    },
+    stagingEnvironment: 'https://test-staging-environment',
+    visualisation: 'test-visualization',
+    readOnly: true,
+    hub: { id: 'hubId', name: 'hubName' },
+  };
+
+  const options = {
+    window: window,
+    connectionTimeout: false,
+    timeout: false,
+    debug: false,
+  };
+
   it('should create a new instance of ContentEditorExtension', () => {
-    const options = {
-      window: window,
-      connectionTimeout: false,
-      timeout: false,
-      debug: false,
-    };
     const connection = new ClientConnection(options);
-    const instance = new ContentEditorExtension({ connection, ...options });
+    const instance = new ContentEditorExtension({ connection, ...options }, context);
 
     expect(instance).toBeInstanceOf(ContentEditorExtension);
   });
 
   describe('ContentEditorExtension.setupContext', () => {
     it('should setup up the ContentEditorExtension instance context properties', () => {
-      const options = {
-        window: window,
-        connectionTimeout: false,
-        timeout: false,
-        debug: false,
-      };
-      const context = {
-        category: 'CONTENT_EDITOR',
-        contentItemId: '12345678-abcd-1234-1234-abcdef123456',
-        schema: {
-          id: 'http://simple.com',
-          $schema: 'http://json-schema.org/draft-07/schema#',
-          allOf: [
-            {
-              $ref: 'http://bigcontent.io/cms/schema/v1/core#/definitions/content',
-            },
-          ],
-          title: 'test-field-schema-title',
-          type: 'test-field-schema-type',
-          description: 'test-field-schema-desc',
-          properties: {
-            title: {
-              type: 'string',
-            },
-          },
-        },
-        params: {
-          instance: {},
-          installation: {},
-        },
-        locales: {
-          default: ['en'],
-          available: [{ locale: 'en-gb', language: 'en', country: 'gb', index: 1, selected: true }],
-        },
-        stagingEnvironment: 'https://test-staging-environment',
-        visualisation: 'test-visualization',
-        readOnly: true,
-        hub: { id: 'hubId', name: 'hubName' },
-      };
-
       const connection = new ClientConnection(options);
-      const instance = new ContentEditorExtension({ connection, ...options });
+      const instance = new ContentEditorExtension({ connection, ...options }, context);
 
       expect(instance.connection).toEqual(connection);
 
       expect(instance.mediaLink).toBeInstanceOf(MediaLink);
       expect(instance.contentLink).toBeInstanceOf(ContentLink);
       expect(instance.contentReference).toBeInstanceOf(ContentReference);
-
-      instance.setupContext(context);
-
       expect(instance.contentItem).toBeInstanceOf(ContentItem);
       expect(instance.form).toBeInstanceOf(ContentEditorForm);
       expect(instance.params).toEqual(context.params);
