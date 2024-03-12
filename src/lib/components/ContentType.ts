595f03a7d129a6727f00d969a294801ba037e820
Joe Warner <jwarner@amplience.com>
2024-03-12 15:56:04 +0000

    fix: removed id

--- src/lib/components/ContentType.ts
+++ src/lib/components/ContentType.ts
@@ -4,11 +4,11 @@ import { ClientConnection } from 'message-event-channel';
 export class ContentTypes {
   constructor(private connection: ClientConnection) {}
 
-  async getByUri(id: string, uri: string) {
-    return this.connection.request(CONTENT_TYPES.GET_BY_URI, { id, uri });
+  async getByUri(uri: string) {
+    return this.connection.request(CONTENT_TYPES.GET_BY_URI, { uri });
   }
 
-  async getByUris(id: string, uris: string[]) {
-    return this.connection.request(CONTENT_TYPES.GET_BY_URIS, { id, uris });
+  async getByUris(uris: string[]) {
+    return this.connection.request(CONTENT_TYPES.GET_BY_URIS, { uris });
   }
 }
